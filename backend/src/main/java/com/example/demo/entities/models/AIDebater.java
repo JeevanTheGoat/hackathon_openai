e528e0f5ac2c88c46f9de893496229d7c388b9f3
JeevanTheGoat <158585514+JeevanTheGoat@users.noreply.github.com>
2025-09-07 12:30:08 -0400

    Update AIDebater.java

--- backend/src/main/java/com/example/demo/entities/models/AIDebater.java
+++ backend/src/main/java/com/example/demo/entities/models/AIDebater.java
@@ -1,6 +1,5 @@
 package com.example.demo.entities.models;
 
-
 import com.example.demo.entities.enums.Strength;
 import com.example.demo.entities.enums.Style;
 import lombok.Getter;
@@ -9,19 +8,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AIDebater {
-
     private long id;
-
     private Strength strength;
-
     private Style style;
-
     private int funnyVotes;
-
     private int creativeVotes;
-
     private int wins;
-
     private int played;
-
 }
