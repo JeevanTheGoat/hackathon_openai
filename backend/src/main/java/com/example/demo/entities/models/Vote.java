9a00f8ca19bef145fc3d3e018e16b12eef6aa7a9
JeevanTheGoat <158585514+JeevanTheGoat@users.noreply.github.com>
2025-09-07 12:30:42 -0400

    Update Vote.java

--- backend/src/main/java/com/example/demo/entities/models/Vote.java
+++ backend/src/main/java/com/example/demo/entities/models/Vote.java
@@ -1,18 +1,12 @@
 package com.example.demo.entities.models;
 
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Vote {
-
     private String bestArgument;
-
     private String funniest;
-
     private String mostCreative;
-
 }
