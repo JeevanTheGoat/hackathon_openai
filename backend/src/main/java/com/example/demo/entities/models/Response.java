117cdff869b5be2b8e5a55a96076caeb8488ff4f
JeevanTheGoat <158585514+JeevanTheGoat@users.noreply.github.com>
2025-09-07 12:30:31 -0400

    Update Response.java

--- backend/src/main/java/com/example/demo/entities/models/Response.java
+++ backend/src/main/java/com/example/demo/entities/models/Response.java
@@ -1,30 +1,22 @@
 package com.example.demo.entities.models;
 
-
 import com.example.demo.entities.enums.DebateRound;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-
 @Getter
 @Setter
 public class Response {
-
-
     private long id;
 
     @JsonBackReference
     private Debate debate;
 
     private String sender;
-
     private String content;
-
     private DebateRound round;
-
     private LocalDateTime createdAt = LocalDateTime.now();
 }
