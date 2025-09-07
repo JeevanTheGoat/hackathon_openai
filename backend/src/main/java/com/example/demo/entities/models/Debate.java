351ff7f431b5ee9110328f3e20a4fab91820d5df
JeevanTheGoat <158585514+JeevanTheGoat@users.noreply.github.com>
2025-09-07 12:25:14 -0400

    Update Debate.java

--- backend/src/main/java/com/example/demo/entities/models/Debate.java
+++ backend/src/main/java/com/example/demo/entities/models/Debate.java
@@ -1,12 +1,9 @@
 package com.example.demo.entities.models;
 
-
 import com.example.demo.entities.enums.DebateRound;
 import com.example.demo.entities.enums.DebateStatus;
 import com.example.demo.entities.enums.DebateWinner;
-import com.example.demo.service.DebateService;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,35 +13,30 @@ import java.util.*;
 @Setter
 public class Debate {
 
-    public Debate(){
+    public Debate() {
         roundsData.put("opening", new ArrayList<>());
         roundsData.put("rebuttal", new ArrayList<>());
         roundsData.put("crosstalk", new ArrayList<>());
         roundsData.put("closing", new ArrayList<>());
+        user_messages = new HashMap<>();
+        votes = new ArrayList<>();
     }
 
-
     private long id;
-
     private String topic;
 
     @JsonManagedReference
-    private List<Vote> votes = new ArrayList<>();
+    private List<Vote> votes;
 
     @JsonManagedReference
-    private Map<String, List<Response>> roundsData = new HashMap<>();
+    private Map<String, List<Response>> roundsData;
 
     private int nextMessageId = 1;
-
     private boolean user_participated;
-
     private Map<String, String> user_messages;
-
     private List<AIDebater> debaters = new ArrayList<>();
 
     private DebateStatus status = DebateStatus.PENDING;
     private DebateWinner winner = DebateWinner.NONE;
     private DebateRound round = DebateRound.OPENING;
-
-
 }
